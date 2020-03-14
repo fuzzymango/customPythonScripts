370428b0768831ccdf8e6f7ca18e885a825de401
fuzzymango <46225294+fuzzymango@users.noreply.github.com>
2020-03-14 13:13:16 -0500

    Update setProjectSettings.py

--- setProjectSettings.py
+++ setProjectSettings.py
@@ -1,113 +1,142 @@
 # --------------------------------------------------------------
 #  setProjectSettings.py
-#  Version: 0.2.2
-#  Last Updated: March 10th 2020
-#  Tested on Nuke 11
+#  Version: 0.2.3
+#  Last Updated: March 14th 2020
+#  Tested on Windows Nuke 11.2v4
 # --------------------------------------------------------------
 import nuke 
 import nukescripts
 import re
 
+
 def projectSettings():
-	# get selected node(s)
-	try:
-		n = nuke.selectedNodes()
-	except:
-		nuke.message('You must select a node')
+    # get selected node(s)
+    try:
+        n = nuke.selectedNodes()
+    except:
+        nuke.message('You must select a node')
 
-	# see if there is only 1 node selected 
-	if len(n) is 1: 
-		# check if the selected node is a read
-		if n[0].Class() == 'Read':
-			read = n[0]
+    # see if there is only 1 node selected 
+    if len(n) is 1: 
+        # -----------------------------------------------------------------------------------------------------------------------------------------
+        # GATHER INFORMATION ABOUT THE USER SELECTED NODE
+        # -----------------------------------------------------------------------------------------------------------------------------------------
+        # check if the selected node is a read
+        if n[0].Class() == 'Read':
+            read = n[0]
 
-			# get the frame range
-			fr_first = int(read['first'].getValue())
-			fr_last = int(read['last'].getValue())
+            # get the frame range
+            fr_first = str(read['first'].getValue())
+            fr_last = str(read['last'].getValue())
 
-			# get the resolution 
-			res_width = read.width()
-			res_height = read.height()
+            # get the resolution 
+            res_width = read.width()
+            res_height = read.height()
 
-			# get pixel aspect ratio
-			pixel_aspect = read.pixelAspect()
+            # get pixel aspect ratio
+            pixel_aspect = read.pixelAspect()
 
-			# get current format string
-			current_proj_format = nuke.knob('root.format')
+            # get current format string
+            current_proj_format = nuke.knob('root.format')
 
-			# create format string
-			new_proj_format = '%s %s 0 0 %s %s %s' % (res_width, res_height, res_width, res_height, pixel_aspect)
-
-			# get a list of all currently loaded formats
-			nameless_format_list = []
-			named_format_list = []
-			scriptFormats = nuke.formats()
-			for f in scriptFormats:
-			    new_nameless = '%s %s 0 0 %s %s %s' % (f.width(), f.height(), f.width(), f.height(), f.pixelAspect())
-			    new_named = '%s %s 0 0 %s %s %s %s' % (f.width(), f.height(), f.width(), f.height(), f.pixelAspect(), f.name())
-			    nameless_format_list.append(new_nameless)
-			    named_format_list.append(new_named)
+            # create format string
+            new_proj_format = '%s %s 0 0 %s %s %s' % (res_width, res_height, res_width, res_height, pixel_aspect)
+        # -----------------------------------------------------------------------------------------------------------------------------------------
 
 
-			# check to see if the new project format matches a current project format
-			for i in nameless_format_list:
-				print '\ni: %s \nnew format: %s\n' % (i, new_proj_format)
-				# if there's a match, set the project format and display the completion message
-				# if i = new project format and i does not contain any letters
-				print 'regex: %s\ni: %s' % (re.search('[a-zA-Z]', i), i)
-				if i == new_proj_format and re.search('[a-zA-Z]', named_format_list[named_format_list.index(i)]):
-						# confirm with the user that all the settings are correct
-						askMessage = 'frame range: %s - %s\nFull size format: %s\nConfirm?' % (fr_first, fr_last, new_proj_format)
-						if nuke.ask(askMessage):
-							# set the project format to the new project format
-							nuke.knob('root.format', new_proj_format)
+        # -----------------------------------------------------------------------------------------------------------------------------------------
+        # CREATE A LIST OF ALL CURRENTLY LOADED ROOT FORMATS
+        # -----------------------------------------------------------------------------------------------------------------------------------------
+            # get a list of all currently loaded formats
+            # one list has all information except for the name, the other has all information including the name of the format
+            nameless_format_list = []
+            named_format_list = []
 
-							# set the frist/last frame
-							nuke.knob('root.first_frame', str(fr_first))
-							nuke.knob('root.last_frame', str(fr_last))
-							
-							return
-						else:
-							return
+            # get a list of all the loaded nuke formats
+            scriptFormats = nuke.formats()
+
+            # loop for all formats to add entries to the lists containing resolution, asepct ratio, and name (for the named list only)
+            for f in scriptFormats:
+                new_nameless = '%s %s 0 0 %s %s %s' % (f.width(), f.height(), f.width(), f.height(), f.pixelAspect())
+                new_named = '%s %s 0 0 %s %s %s %s' % (f.width(), f.height(), f.width(), f.height(), f.pixelAspect(), f.name())
+                nameless_format_list.append(new_nameless)
+                named_format_list.append(new_named)
+        # -----------------------------------------------------------------------------------------------------------------------------------------
 
 
-			# if there is not a match, request the user to enter a name for the new format
-			# prompt user to enter name of new format and save that as a string called txt
-			messageText = 'New format detected! Enter a name for the new format'
-			txt = nuke.getInput(messageText, 'new_format')
+        # -----------------------------------------------------------------------------------------------------------------------------------------
+        # CHECK IF THE INCOMING FORMAT IS ALREADY IN THE LOADED LIST OF FORMATS
+        # -----------------------------------------------------------------------------------------------------------------------------------------
+            # loop through each entry inthe nameless list
+            for i in range(len(nameless_format_list)):
 
-			# check to see if the user left the field blank
-			if txt is '':
-				message = nuke.message('give your format a name')
+                # if an entry in the nameless list matches the project format from the selected read node
+                # and if that entry does not contain the text 'None' at the end
+                if nameless_format_list[i] == new_proj_format and re.search('None$', named_format_list[i]) == None:
+                    #print '\nnew_proj_format: %s\nnamless0%s: %s\nnamed0%s: %s\nregex: %s' % (new_proj_format, i, nameless_format_list[i], i, named_format_list[i], re.search('None$', named_format_list[i]))
 
-				return
-			else: 
-				# append the name of the new project format
-				try:
-					new_proj_format = new_proj_format + ' ' + txt
-				# catch a type error that throws when the user cancels the format name box
-				except TypeError as e:
-					return
+                    # confirm with the user that all the settings are correct
+                    askMessage = 'frame range: %s - %s\nFull size format: %s\nConfirm?' % (fr_first, fr_last, new_proj_format)
+                    if nuke.ask(askMessage):
 
-				# confirm with the user that all the settings are correct
-				askMessage = 'frame range: %s - %s\nFull size format: %s\nConfirm?' % (fr_first, fr_last, new_proj_format)
-				if nuke.ask(askMessage):
-					# set the project format to the new project format
-					nuke.knob('root.format', new_proj_format)
+                        # set the project format to the new project format
+                        nuke.knob('root.format', new_proj_format)
 
-					# set the frist/last frame
-					nuke.knob('root.first_frame', str(fr_first))
-					nuke.knob('root.last_frame', str(fr_last))
+                        # set the frist/last frame
+                        nuke.knob('root.first_frame', fr_first)
+                        nuke.knob('root.last_frame', fr_last)
+                        return
+                    else:
+                        return
+        # -----------------------------------------------------------------------------------------------------------------------------------------
 
-					return
-				# if the user cancels, exit the script
-				else:
-					return
 
-			# wrong node class selected
-			nuke.message('Wrong node class selected')
-	else: 
-		# too many nodes selected 
-		nuke.message('Select only one Read node')
+        # -----------------------------------------------------------------------------------------------------------------------------------------
+        # PROMT THE USER TO CREATE A NEW ROOT FORMAT
+        # -----------------------------------------------------------------------------------------------------------------------------------------
+            # if there is not a match, request the user to enter a name for the new format
+            # prompt user to enter name of new format and save that as a string called txt
+            messageText = 'New format detected! Enter a name for the new format'
+            txt = nuke.getInput(messageText, 'new_format')
+
+            # check to see if the user left the field blank
+            if txt is '':
+                message = nuke.message('give your format a name')
+
+                return
+            else: 
+                # append the name of the new project format
+                try:
+                    new_proj_format = new_proj_format + ' ' + txt
+                # catch a type error that throws when the user cancels the format name box
+                except TypeError as e:
+                    return
+
+                # confirm with the user that all the settings are correct
+                askMessage = 'frame range: %s - %s\nFull size format: %s\nConfirm?' % (fr_first, fr_last, new_proj_format)
+                if nuke.ask(askMessage):
+                    # set the project format to the new project format
+                    nuke.knob('root.format', new_proj_format)
+
+                    # set the frist/last frame
+                    nuke.knob('root.first_frame', fr_first)
+                    nuke.knob('root.last_frame', fr_last)
+
+                    return
+                # if the user cancels, exit the script
+                else:
+                    return
+        # -----------------------------------------------------------------------------------------------------------------------------------------
+
+
+        # -----------------------------------------------------------------------------------------------------------------------------------------
+        # CATCH THE USER SELECTING THE WRONG INPUTS
+        # -----------------------------------------------------------------------------------------------------------------------------------------
+            # wrong node class selected
+            nuke.message('Wrong node class selected')
+    else: 
+        # too many nodes selected 
+        nuke.message('Select only one Read node')
+        # -----------------------------------------------------------------------------------------------------------------------------------------
 
 projectSettings()
